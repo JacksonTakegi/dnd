86a9533cf6617df5398bbecf195adbbb0b839583
John Hollingsworth <hollingsfsdf@gmail.com>
2019-04-14 23:17:27 -0600

    Add delete user test
    Update methods so phpunit sees them
    
    
    whoops
    
    
    a
    
    
    b
    
    
    Man i wish I had docker on this laptop
    
    
    People II
    
    
    People III
    
    
    12345

--- tests/Unit/CombatTest.php
+++ tests/Unit/CombatTest.php
@@ -5,17 +5,30 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use \App\User;
 
 class CombatTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use RefreshDatabase;
+
+    /** @test */
+    public function testAddCharacter()
     {
     	$this->post('/combat/add', ['character' => 'test name', 'roll' => 10]);
     	$this->assertDatabaseHas('users', ['name' => 'test name', 'roll' => 10]);
     }
+
+    /** @test */
+    public function testDeleteCharacter()
+    {
+        $user = new User;
+        $user->name = "aaaa";
+        $user->roll = 10;
+        $user->turn = false;
+        $user->save();
+
+        $this->get('/combat/delete/'. $user->id);
+        $this->assertDatabaseMissing('users',['id' => $user->id]);
+    }
+
 }
