8b2985a7bfd6b4658b984cdcae054eb8ee2d67a0
Hollings <hollingsfsdf@gmail.com>
2019-04-24 18:22:44 -0600

    make the links

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,5 +1,16 @@
-<div class="row">
+<style>
+    .navigation-link {
+        display: inline;
+        margin: 0 30px;
+    }
+    .navigation-container {
+        background-color: #c5c5c5;
+        box-shadow: 1px 1px 5px;
+    }
+</style>
+
+<div class="row navigation-container">
     <div class="col-md-12">
-    test | test 2
+        <div class="navigation-link"><a href="/characters">Characters</a></div><div class="navigation-link"><a href="/combat">Combat</a></div></div>
     </div>
 </div>
